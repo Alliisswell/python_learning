596fbd3e11dfa7829cc9acad046bf9df32a954dd
li-xiaoning <94219204+Alliisswell@users.noreply.github.com>
2023-02-26 21:58:48 +0800

    Delete 廖雪峰 directory

--- 廖雪峰/chap002_函数式编程/lxf_01_高阶函数/lxf_02_filter.py
+++
@@ -1,8 +0,0 @@
-# 妙啊！！！利用切片，反向遍历
-def is_palindrome(n):
-    num = str(n)
-    return num == num[::-1]
-
-
-output = list(filter(is_palindrome, range(1, 201)))
-print('1~200内的回数：', output)
--- 廖雪峰/chap002_函数式编程/lxf_03_匿名函数.py
+++
@@ -1,7 +0,0 @@
-L = list(filter(lambda n: n % 2 == 1, range(1, 20)))
-
-print(L)
-"""
-小结：
-Python对匿名函数的支持有限，只有一些简单的情况下可以使用匿名函数。
-"""
