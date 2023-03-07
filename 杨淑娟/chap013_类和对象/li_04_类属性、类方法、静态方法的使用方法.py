54032d05e68ba9c2669c9d1102b9bbc19e91c945
li-xiaoning <94219204+Alliisswell@users.noreply.github.com>
2023-03-07 16:58:19 +0800

    Add files via upload

--- 杨淑娟/chap013_类和对象/li_04_类属性、类方法、静态方法的使用方法.py
+++ 杨淑娟/chap013_类和对象/li_04_类属性、类方法、静态方法的使用方法.py
@@ -11,16 +11,16 @@ class Student:
         self.name = name  # 将局部变量name的值赋给实例属性self.name
         self.age = age
 
-    # 实例方法
+    # 实例方法，不能直接调用，需要先进行实例化
     def eat(self):
         print('学生在吃饭')
 
-    # 静态方法
+    # 静态方法，直接调用，不需要实例化
     @staticmethod
     def sm():
         print('我使用了staticmethod进行修饰，所以我是静态方法')
 
-    # 类方法
+    # 类方法，直接调用，不需要实例化
     @classmethod
     def cm(cls):
         print('我使用了classmethod进行修饰，所以我是类方法')
@@ -35,7 +35,11 @@ print(stu2.native_pace)
 Student.native_pace = '天津'
 print(stu1.native_pace)
 print(stu2.native_pace)
+
+# 静态方法的调用
 print('*' * 5, '调用静态方法', '*' * 5)
 Student.sm()
+
+# 类方法的调用
 print('*' * 5, '调用类方法', '*' * 5)
 Student.cm()
