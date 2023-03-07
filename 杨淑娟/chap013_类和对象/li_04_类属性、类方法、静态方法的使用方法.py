7d176ee45c2fa5654a962b2894bb3a8cad6c2542
li-xiaoning <94219204+Alliisswell@users.noreply.github.com>
2023-03-07 17:20:45 +0800

    Add files via upload

--- 杨淑娟/chap013_类和对象/li_04_类属性、类方法、静态方法的使用方法.py
+++ 杨淑娟/chap013_类和对象/li_04_类属性、类方法、静态方法的使用方法.py
@@ -16,32 +16,36 @@ class Student:
     def eat(self):
         print('学生在吃饭')
 
-    # 静态方法，直接调用，不需要实例化
-    @staticmethod
+    # 静态方法，直接调用，不需要提前实例化
+    @staticmethod  # 定义静态方法需要使用@staticmethod装饰器(decorator)
     def sm():
         print('我使用了staticmethod进行修饰，所以我是静态方法')
 
-    # 类方法，直接调用，不需要实例化
-    @classmethod
+    # 类方法，直接调用，不需要提前实例化
+    @classmethod  # 定义类方法需要使用@classmethod装饰器(decorator)
     def cm(cls):
         print('我使用了classmethod进行修饰，所以我是类方法')
 
 
-# 通过实例访问静态属性
-stu1 = Student('张三', 20)
-stu2 = Student('李四', 30)
-print(stu1.native_pace)
-print(stu2.native_pace)
-# 通过类修改静态属性
-Student.native_pace = '天津'
-print(stu1.native_pace)
-print(stu2.native_pace)
-
 # 静态方法的调用
 print('*' * 5, '调用静态方法', '*' * 5)
 Student.sm()
+stu1 = Student('张三', 20)
+stu1.sm()
 
 # 类方法的调用
 print('*' * 5, '调用类方法', '*' * 5)
 Student.cm()
-stu1.cm()
+stu2 = Student('李四', 30)
+stu2.cm()
+
+# 通过实例访问静态属性
+
+stu3 = Student('王五', 40)
+stu4 = Student('麻子', 50)
+print(stu3.native_pace)
+print(stu4.native_pace)
+# 通过类修改静态属性
+Student.native_pace = '天津'
+print(stu3.native_pace)
+print(stu4.native_pace)
